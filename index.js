187c8ec9161a0b66336e9f162754bf7dd34dcfcb
Alina <alina.sikorski@yahoo.de>
2024-10-05 21:51:08 +0200

    updated axios

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "b37967db0ea6a2t18o10f60a49513fb0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
